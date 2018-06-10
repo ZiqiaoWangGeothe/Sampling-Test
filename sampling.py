8ea7c979d45601b3e36d400ebfc061ab70d3c43c
ziqiaowang <zwang286@uottawa.ca>
2018-06-09 21:55:09 -0400

    add all

---
+++ sampling.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: wangz
+"""
+
+import random
+
+dist = [0.1, 0.2, 0.2, 0.05, 0.05, 0.4]
+
+def sampling(dist):
+    d = dist.copy()
+    l = len(d)
+    for i in range(l):
+        if i > 0:
+            d[i] += d[i-1]
+    u = random.random() * d[-1]
+    for i in range(l):
+        if d[i] >= u:
+            break
+    return i+1,d,u
+
+times = [0]*6
+for i in range(100001):
+    result,d,u = sampling(dist)
+    times[result - 1] += 1
+
+print(times)
+print(times[0]/100000,times[1]/100000,times[2]/100000,times[3]/100000,times[4]/100000,times[5]/100000)
+#print(d,u)        
